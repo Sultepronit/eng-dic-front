05b5908a6dbf9fa9e3acee69dfbc4cbdecc52ab2
Sultepronit <sultepronit@gmail.com>
2024-07-01 17:25:17 +0300

    now we will use localhost as one of the servers! +little changes

--- src/services/getArticle.js
+++ src/services/getArticle.js
@@ -1,12 +1,20 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
 async function getArticle(dic, query) {
-    const url = `${apiUrl}/${dic}?word=${encodeURIComponent(query)}`;
-    console.log(url);
+    const urls = [
+        `http://localhost:8989/?dic=${dic}&word=${encodeURIComponent(query)}`,
+        `${apiUrl}/${dic}?word=${encodeURIComponent(query)}`
+    ];
+
+    const fetchAll = urls.map(url => fetch(url).then(resp => {
+        if(resp.ok) {
+            return resp;
+        }
+    }));
+
     try {
-        const resp = await fetch(url);
-        const data = await resp.text();
-        return data;
+        const resp = await Promise.any(fetchAll);
+        return await resp.text();
     } catch (error) {
         console.log(error);
         return null;
